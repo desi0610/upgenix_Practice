ab08e9bdf0d30408dc1809ebb303db69c231653c
AmirHesamB <amirh3sam.badiei@gmail.com>
2023-03-04 11:23:19 -0500

    commit project on GitHub

---
+++ src/test/java/com/unofficailProject/pages/BasePage.java
@@ -0,0 +1,78 @@
+package com.unofficailProject.pages;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+public abstract class BasePage {
+
+    //US1
+    @FindBy(xpath = "//span[normalize-space()='Sales']")
+    public WebElement SalesPage;
+
+
+    //US2  , US11
+    @FindBy(xpath = "//span[normalize-space()='Repairs']")
+    public WebElement RepairsPage;
+
+    //US3 , US4 , US7 ,US8 , US9
+    @FindBy(xpath = "(//div[@class='navbar-collapse collapse']//ul)[1]")
+    //div[@class='navbar-collapse collapse']
+    public List<WebElement> mainModules;
+    // optional for user story 4 or 3 or 7  or 8 or 9://span[@class='oe_topbar_name']
+
+    //US5
+    @FindBy(xpath = "(//span[normalize-space()='Point of Sale'])[1]")
+    public WebElement pointOfSalesPage;
+    @FindBy(xpath = "(//span[normalize-space()='Orders'])[3]")
+    public WebElement OrdersButton;
+    @FindBy(xpath = "//th[@class='o_list_record_selector']//input[@type='checkbox']")
+    public WebElement orderRefCheckBox;
+    @FindBy(xpath = "//button[normalize-space()='Action']")
+    public WebElement actionButton;
+
+    //U6
+    @FindBy(xpath = "//span[@class='oe_topbar_name']")
+    public WebElement profileButton;
+    @FindBy(xpath = "//a[.='Documentation']")
+    public WebElement docPage;
+    @FindBy(xpath = "//h1[@class='text-white']")
+    public WebElement odooDoc;
+
+    //section[@class='o_content_fw_banner']/following-sibling::* select all the topic
+    @FindBy(xpath = "(//a[.='User Docs'])[2]")
+    public WebElement userDocTopic;
+    @FindBy(xpath = "(//a[.='Install and Maintain'])[2]")
+    public WebElement installAndMaintainTopic;
+    @FindBy(xpath = "(//a[.='Developer'])[2]")
+    public WebElement developerTopic;
+    @FindBy(xpath = "(//a[.='Contributing'])[2]")
+    public WebElement contributingTopic;
+
+    //US10
+    @FindBy(xpath = "(//span[normalize-space()='Quotations'])[2]")
+    public WebElement quotationsButton;
+    @FindBy(xpath = "//th[@class='o_list_record_selector']//input[@type='checkbox']")
+    public WebElement quotationsBox;
+
+    //US11
+    @FindBy(xpath = "//th[@class='o_list_record_selector']//input[@type='checkbox']")
+    public WebElement repairsBox;
+
+    //US14
+    @FindBy(xpath = "//span[normalize-space()='Calendar']")
+    public WebElement calenderPage;
+    //example we want to choose time 0:00 ==>((//tbody)[5]/tr/td/span)[1]
+    @FindBy(xpath = "(//tbody)[5]/tr/td/span")
+    public List<WebElement> timeList;
+
+
+}
---
+++ src/test/java/com/unofficailProject/step_definitions/LoginStepDefs.java
@@ -0,0 +1,53 @@
+package com.unofficailProject.step_definitions;
+
+import com.unofficailProject.pages.LoginPage;
+import com.unofficailProject.utilities.ConfigurationReader;
+import io.cucumber.java.en.Given;
+public class LoginStepDefs {
+
+
+    @Given("the user is on the login page")
+    public void the_user_is_on_the_login_page() {
+        System.out.println("Login to app in Before method");
+    }
+
+    @Given("the user logged in as {string}")
+    public void the_user_logged_in_as(String userType) {
+        //based on input enter that user information
+        String username =null;
+        String password =null;
+
+        if(userType.equalsIgnoreCase("POS_Manager")){
+            username = ConfigurationReader.getProperty("posmanager6@info.com");
+            password = ConfigurationReader.getProperty("posmanager");
+        }else if(userType.equalsIgnoreCase("sales_Manager")){
+            username = ConfigurationReader.getProperty("salesmanager10@info.com");
+            password = ConfigurationReader.getProperty("salesmanager");
+        }else if(userType.equalsIgnoreCase("CRM_Manager")){
+            username = ConfigurationReader.getProperty("eventscrmmanager10@info.com");
+            password = ConfigurationReader.getProperty("eventscrmmanager");
+        }else if(userType.equalsIgnoreCase("Inventory_Manager")) {
+            username = ConfigurationReader.getProperty("imm10@info.com");
+            password = ConfigurationReader.getProperty("inventorymanager");
+
+
+        }else if(userType.equalsIgnoreCase("Expenses_Manager")) {
+            username = ConfigurationReader.getProperty("expensesmanager@info.com");
+            password = ConfigurationReader.getProperty("expensesmanager");
+        }
+
+            //send username and password and login
+        new LoginPage().login(username,password);
+    }
+
+    @Given("the user logged in with username as {string} and password as {string}")
+    public void the_user_logged_in_with_username_as_and_password_as(String username, String password) {
+      LoginPage loginPage=new LoginPage();
+      loginPage.login(username,password);
+    }
+
+
+
+
+
+}
